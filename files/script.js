d9bb88eed0e35e00968759c1d720d3bc1b6d2443
Dipendra Kumar Yadav <dipendrayadav299@gmail.com>
2025-08-10 02:11:38 +0530

    made changes in main file of this project.

--- files/script.js
+++ files/script.js
@@ -1,17 +1,23 @@
-function sendEmail(){
-    // console.log('Hello testing');
+// Contact form function
+function sendEmail() {
     Email.send({
-        SecureToken : "176fa54d-90bd-4fd5-bec9-0aa78a7fde3d",
-        // Host : "smtp.gmail.com",
-        // Username : "yadavdipendra766@gmail.com",
-        // Password : "",
-        To : 'deepydv593@gmail.com',
-        From :document.getElementById('email').value,
-        Subject : "New Form Enquiry",
-        Body : "Name: "+document.getElementById('name').value
-        +"<br> Email: "+document.getElementById('email').value
-        +"<br> Message: "+document.getElementById('message').value
+        SecureToken: "176fa54d-90bd-4fd5-bec9-0aa78a7fde3d", // from smtpjs.com
+        To: 'deepydv593@gmail.com',
+        From: document.getElementById('email').value,
+        Subject: "New Form Enquiry",
+        Body:
+            "Name: " + document.getElementById('name').value +
+            "<br> Email: " + document.getElementById('email').value +
+            "<br> Message: " + document.getElementById('message').value
     }).then(
-      message => alert("Message Sent Successfully.")
+        message => {
+            if (message === "OK") {
+                alert("Message Sent Successfully.");
+                document.querySelector("form").reset();
+            } else {
+                alert("Error: " + message);
+            }
+        }
     );
 }
+
